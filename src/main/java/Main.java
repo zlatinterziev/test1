2bb0ed5f14f467e32b530f245857b466fe0bedec
Zlatin Terziev <zterziev@gmail.com>
2022-10-16 11:07:16 +0300

    sr-0: adding connection to redis.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisURI;
+
 public class Main {
     public static void main(String[] args) {
 
-        var blah = 5;
+        RedisClient redisClient = new RedisClient(
+                RedisURI.create("redis://redispw@localhost:49153"));
+        var connection = redisClient.connect().sync();
 
-        System.out.println("Hello world!" + blah);
+        connection.ping();
+
+        System.out.println("Connected to Redis");
+
+        connection.set("foo", "bar");
+        String value = connection.get("foo");
+
+        connection.set("kur", "bar");
+
+        System.out.println(value);
     }
 }
